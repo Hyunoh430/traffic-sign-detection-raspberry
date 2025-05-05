54960f436e9faf030d0c43722948dbf19dfaa296
현오홍 <harrymickey@naver.com>
2025-05-05 21:50:18 +0900

    evalutation code, yolo,cnn run.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,28 +1,36 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.utils import plot_model
-import os
+for img_path in image_paths:
+    base = os.path.basename(img_path)
+    label_path = os.path.join(label_dir, base + ".txt")
 
-# 모델 정의
-model = keras.Sequential([
-    layers.Input(shape=(28, 28, 1)),
-    layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-    layers.MaxPooling2D(pool_size=(2, 2)),
-    layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-    layers.MaxPooling2D(pool_size=(2, 2)),
-    layers.Flatten(),
-    layers.Dropout(0.5),
-    layers.Dense(10, activation="softmax")
-])
+    if not os.path.exists(label_path):
+        continue
 
-# 현재 작업 디렉토리 출력
-print("현재 작업 디렉토리:", os.getcwd())
+    with open(label_path, "r") as f:
+        lines = f.readlines()
+        if not lines:
+            continue
+        gt_class_raw = select_main_label(lines)
 
-# 모델 구조 시각화 및 저장
-plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+    if gt_class_raw not in gtsdb_to_cnn_class:
+        continue  # CNN이 처리할 수 없는 클래스
 
-print("모델 구조가 'model_plot.png' 파일로 저장되었습니다.")
+    gt_class = gtsdb_to_cnn_class[gt_class_raw]
 
-# 모델 요약 정보 출력
-model.summary()
+    pred_label = detect_and_predict_tflite(
+        weights='128_128.pt',
+        img_path=img_path,
+        crop_save_path='temp_crop.jpg'
+    )
+
+    if pred_label is None:
+        continue
+
+    pred_class = int(pred_label) // 10 - 3
+
+    if pred_class == gt_class:
+        correct += 1
+    total += 1
+
+print(f"\n총 평가 개수: {total}")
+print(f"정답 수: {correct}")
+print(f"정확도: {correct / total:.4f}")
